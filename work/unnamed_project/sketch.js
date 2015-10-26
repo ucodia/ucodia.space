6ad56663da56fc17cc93cdc57f84227976bd82cb
Lionel Ringenbach <ucodia@live.com>
2015-10-26 23:21:01 +0100

    Added basis for unnamed project

---
+++ work/unnamed_project/sketch.js
@@ -0,0 +1,12 @@
+// unnamed project by lionel ringenbach @ ucodia.io
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+}
+
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
+}
