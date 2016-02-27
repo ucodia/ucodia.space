5693f1e33e9d16649bc0435c68307eb2182989de
Lionel Ringenbach <ucodia@live.com>
2016-02-28 00:00:39 +0100

    Added gradients dataset and automatic styling

---
+++ work/kinetic_project/app.js
@@ -0,0 +1,49 @@
+(function() {
+    $.getJSON("gradients.json", function(data) {
+        var index = null;      
+        try { index = JSON.parse(getParameterByName("style")); }
+        catch (error) {}
+        
+        if (typeof(index) !== 'number' || index < 0 || index > data.length - 1)
+            index = getRandomInt(0, data.length - 1);
+        
+        var gradient = data[index];
+        
+        var svgGradients = [
+            d3.select("#zinnia1-gradient"),
+            d3.select("#zinnia2-gradient")
+        ];
+        
+        for (var i = 0; i < svgGradients.length; i++) {
+            var svgGradient = svgGradients[i];
+            setGradient(svgGradient, gradient);
+        }
+    });
+    
+    function setGradient(svgGradient, gradient) {
+        var step = gradient.colors.length === 2 ? 1 :1 / gradient.colors.length - 1;
+        
+        for (var i = 0; i < gradient.colors.length; i++) {
+            var color = gradient.colors[i];
+            svgGradient.append('stop')
+                .attr({
+                    offset: function() { return i * step },
+                    "stop-color": color
+                });
+        }
+    }
+    
+    // utilities
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+        var results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+})()
