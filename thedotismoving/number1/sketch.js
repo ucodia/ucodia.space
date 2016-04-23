760c9980da0691f1329c7fa05e9bfcf99eea1108
Lionel Ringenbach <ucodia@live.com>
2016-04-23 11:39:42 +0200

    Update to thedotismoving

--- thedotismoving/number1/sketch.js
+++ thedotismoving/number1/sketch.js
@@ -11,8 +11,12 @@ var dots = [
     { x: 575, y: 575 }
 ];
 
+var n = 8;
+var space = 100;
+var dots2 = [];
+
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(window.innerWidth, window.innerHeight);
     rectMode(CENTER);
     frameRate(8);
     
@@ -24,8 +28,22 @@ function setup() {
     }
 }
 
+// function layout() {
+//     for (var i = 0; i < n; i++) {
+//         dots2[i] = [];
+//         var n2 = i % 2 === 0 ? n : n - 1;
+//         var offset = i % 2 === 0 ? 0 : space / 2;
+//         for (var j = 0; j < n2; j++) {
+//             var dots2[i][j] = {
+//                 x: i * i * space,
+//                 y: j * i
+//             };
+//         }
+//     }
+// }
+
 function draw() {
-    background(235);
+    clear();
     
     for (var i = 0; i < dots.length; i++) {
         var dot = dots[i];
@@ -72,4 +90,9 @@ function square(x, y) {
         draw: draw,
         move: move
     }
+}
+
+function hypot(opp, adj) {
+    if (!adj) adj = opp;
+    return Math.sqrt(Math.pow(opp, 2) + Math.pow(adj));
 }
