85634cccfad539d3403562013d7d758d3e4539d2
Lionel Ringenbach <Lionel Ringenbach>
2016-05-21 17:44:07 +0200

    Added control UI to drawbot001

--- work/drawbot001/sketch.js
+++ work/drawbot001/sketch.js
@@ -8,54 +8,115 @@
 var bots = [];
 var lastCap = 0;
 
+// UI
+var gui = new dat.GUI();
+gui.close();
+
 // settings
 var settings = {};
 settings.back = 20;
-settings.opacity = 50;
-settings.maxBubs = 80;
-settings.minSpeed = 1;
-settings.maxSpeed = 1;
-settings.attDist = 100;
-settings.minSize = 20;
-settings.maxSize = 200;
-settings.popFreq = 1;
-settings.capFreq = 3000;
-settings.dump = false;
+settings.opacity = 5;
+settings.maxBots = 30;
+settings.popFreq = 5;
+settings.minSpeed = 0.2;
+settings.maxSpeed = 2;
+settings.minSize = 10;
+settings.maxSize = 100;
+settings.attDist = 200;
+settings.attract = false;
+settings.reframe = false;
+settings.showBot = false;
+settings.contrast = false;
+settings.autoReset = true;
+settings.resetFreq = 800;
+settings.autoRandomize = false;
+settings.autoCapture = false;
+
+// actions
+var actions = {
+  randomize: randomize,
+  reset: reset,
+  randReset: function () { this.randomize(); this.reset(); },
+  capture: capture
+};
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   frameRate(60);
   colorMode(HSB, 100);
   
-  // set parameters from URL
-  var params = getURLParams();
-  settings.dump =  params.dump ? JSON.parse(params.dump) : false;
-  
+  // define UI controls
+  // graphics
+  var fGraphics = gui.addFolder('Graphics');
+  fGraphics.open();
+  fGraphics.add(settings, 'back', 0, 100);
+  fGraphics.add(settings, 'opacity', 0, 100);
+  fGraphics.add(settings, 'maxBots', 8, 200);
+  fGraphics.add(settings, 'popFreq', 1, 20);
+  fGraphics.add(settings, 'minSpeed', 0, 2);
+  fGraphics.add(settings, 'maxSpeed', 0, 100);
+  fGraphics.add(settings, 'minSize', 1, 20);
+  fGraphics.add(settings, 'maxSize', 20, 100);
+  fGraphics.add(settings, 'attDist', 10, 400);
+  // behaviors
+  var fBehaviors = gui.addFolder('Behaviors');
+  fBehaviors.open();
+  fBehaviors.add(settings, 'attract');
+  fBehaviors.add(settings, 'reframe');
+  fBehaviors.add(settings, 'showBot');
+  fBehaviors.add(settings, 'contrast');
+  fBehaviors.add(settings, 'autoReset');
+  // refresh
+  var fReset = fBehaviors.addFolder('Reset');
+  fReset.add(settings, 'resetFreq', 200, 1500);
+  fReset.add(settings, 'autoRandomize');
+  fReset.add(settings, 'autoCapture');
+  // actions
+  var fActions = gui.addFolder('Actions');
+  fActions.open();
+  fActions.add(actions, 'randomize');
+  fActions.add(actions, 'reset');
+  fActions.add(actions, 'randReset');
+  fActions.add(actions, 'capture');
+
   reset();
 }
 
-function reset() {
-  // randomize settings
+function randomize() {
+  settings.resetFreq = randomInt(200, 1500);
   settings.back = randomInt(0, 100);
   settings.opacity = randomInt(0, 100);
-  settings.maxBubs = randomInt(8, 200);
-  settings.minSpeed = random(0, 2);
-  settings.maxSpeed = randomInt(settings.minSpeed, 100);
-  settings.attDist = randomInt(10, 400);
-  settings.minSize = randomInt(1, 20);
-  settings.maxSize = randomInt(settings.minSize, 100);
+  settings.maxBots = randomInt(8, 200);
   settings.popFreq = randomInt(1, 20);
-  settings.capFreq = randomInt(200, 1500);
-  
-  // behaviors
+  settings.minSpeed = random(0, 2);
+  settings.maxSpeed = random(0, 100);  
+  settings.minSize = random(1, 20);
+  settings.maxSize = random(settings.minSize, 100);
+  settings.attDist = random(10, 400);
   settings.attract = rollDice(2);
   settings.reframe = rollDice(2);
-  settings.showbot = rollDice(8);
+  settings.showBot = rollDice(8);
   settings.contrast = rollDice(12);
   
+  for (var f in gui.__folders) {
+    for (var c in gui.__folders[f].__controllers) {
+      gui.__folders[f].__controllers[c].updateDisplay(); 
+    }
+  }
+}
+
+function reset() {  
+  if(settings.autoRandomize)
+    randomize();
+  
   // reset canvas
+  lastCap = frameCount;
   background(settings.back);
   bots = [];
+}
+
+function capture() {
+  saveCanvas("frame" + getTimestamp(), "jpg");
 }
 
 function draw() {
@@ -108,15 +169,14 @@ function draw() {
   
   // add some bubbles to the bots
   // frameCount % n === 0 is for rate limiting
-  if (bots.length < settings.maxBubs && frameCount % settings.popFreq === 0)
+  if (bots.length < settings.maxBots && frameCount % settings.popFreq === 0)
     bots.push(new Bot(random(width), random(height)));
    
   // check for drawing completion and dump
-  if ((frameCount - lastCap) % settings.capFreq === 0) {
-    if (settings.dump)
-      saveCanvas("frame" + getTimestamp(), "jpg");
-    
-    lastCap = frameCount;
+  if (settings.autoReset && frameCount - lastCap >= settings.resetFreq) {
+    if (settings.autoCapture)
+      capture();
+
     reset();
   }
 }
@@ -178,7 +238,7 @@ function Bot(x, y) {
   
   this.draw = function() {  
     // draw bot positions
-    if (settings.showbot) {
+    if (settings.showBot) {
       noStroke();
       fill(getColor());
       ellipse(this.pos.x, this.pos.y, this.size, this.size);
@@ -215,7 +275,7 @@ function keyPressed() {
 ///////////////
 
 function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return floor(random() * (max - min + 1)) + min;
 }
 
 function rollDice(faces) {
