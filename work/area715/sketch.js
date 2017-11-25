43570a974dbd1771542fd38bf4399690ab767601
Lionel Ringenbach <ringenba@amazon.com>
2017-11-25 03:50:48 -0800

    Removed dependency on p5.utils.js

--- work/area715/sketch.js
+++ work/area715/sketch.js
@@ -262,3 +262,33 @@ function Looper(start, end) {
     this.current = this.start;
   };
 }
+
+// equilateral primitive
+
+function equi(x, y, size, mode) {
+  // the mode changes vertical centering
+  var m = mode | 0;
+  var h = triHeight(size, size / 2);
+  var h3 = h / 3;
+
+  var x1 = x;
+  var x2 = x - size / 2;
+  var x3 = x + size / 2;
+
+  if (mode == 0) {
+    var y1 = y - 2 * h3;
+    var y2 = y + h3;
+    var y3 = y + h3;
+  }
+  else {
+    var y1 = y - h / 2;
+    var y2 = y + h / 2;
+    var y3 = y + h / 2;
+  }
+
+  this.triangle(x1, y1, x2, y2, x3, y3);
+}
+
+function triHeight(hypo, a) {
+  return sqrt(pow(hypo, 2) - pow(a, 2));
+}
