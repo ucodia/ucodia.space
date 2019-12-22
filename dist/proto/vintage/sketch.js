d5ea3b68272afa29cf8eb4e88f855e5fed2c0de2
Lionel Ringenbach <lionel@ucodia.space>
2019-12-21 20:05:02 -0800

    Re-add vintage graphics prototype

---
+++ dist/proto/vintage/sketch.js
@@ -0,0 +1,63 @@
+// vintage by lionel ringenbach @ ucodia.space
+
+var t = 0;
+var n = 100;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+}
+
+function draw() {
+  background(0);
+  translate(width / 2, height / 2);
+  strokeWeight(2);
+
+  for (var i = 0; i < n; i++) {
+    var color = map(i, 0, 360, 0, n * 2);
+    stroke(color, 80, 80, 0.9);
+    var inc = i * 0.5;
+    line(x1(t + inc), y1(t + inc), x2(t + inc), y2(t + inc));
+  }
+
+  for (var i = 0; i < n; i++) {
+    var color = map(i, 360, 0, 0, n * 2);
+    stroke(color, 80, 80, 0.9);
+    var inc = i * 0.5;
+    line(x3(t + inc), y3(t + inc), x4(t + inc), y4(t + inc));
+  }
+
+  t += map(mouseX, 0, width, -4, 4);
+}
+
+function x1(t) {
+  return sin(t / 10) * 300 + sin(t / 10) * 20;
+}
+
+function y1(t) {
+  return cos(t / 10) * 100;
+}
+
+function x2(t) {
+  return sin(t / 10) * 300 + sin(t) * 2;
+}
+
+function y2(t) {
+  return cos(t / 20) * 100 + cos(t / 12) * 200;
+}
+
+function x3(t) {
+  return sin(t / 5) * 200 + sin(t / 7) * 50;
+}
+
+function y3(t) {
+  return cos(t / 10) * 300;
+}
+
+function x4(t) {
+  return sin(t / 10) * 300 + sin(t / 45) * 2;
+}
+
+function y4(t) {
+  return cos(t / 20) * 100 + cos(t / 19) * 200;
+}
