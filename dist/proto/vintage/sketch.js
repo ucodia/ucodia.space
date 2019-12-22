0bcbeb124d876d2da7575deb0143b3a30e36cc99
Lionel Ringenbach <lionel@ucodia.space>
2019-12-21 21:00:51 -0800

    Adding interactions based of mouse position

--- dist/proto/vintage/sketch.js
+++ dist/proto/vintage/sketch.js
@@ -1,7 +1,10 @@
 // vintage by lionel ringenbach @ ucodia.space
 
-var t = 0;
-var n = 100;
+let t = 0;
+let a = 0;
+let b = 0;
+const n = 100;
+const maxSpeed = 2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -9,55 +12,59 @@ function setup() {
 }
 
 function draw() {
+  a = map(mouseY, 0, height, 0.5, 1.5);
+  b = map(mouseX, 0, height, 0.5, 1.5);
+
   background(0);
   translate(width / 2, height / 2);
   strokeWeight(2);
 
-  for (var i = 0; i < n; i++) {
-    var color = map(i, 0, 360, 0, n * 2);
+  for (let i = 0; i < n; i++) {
+    const color = map(i, 0, 360, 0, n * 2);
     stroke(color, 80, 80, 0.9);
-    var inc = i * 0.5;
+    const inc = i * 0.5;
     line(x1(t + inc), y1(t + inc), x2(t + inc), y2(t + inc));
   }
 
-  for (var i = 0; i < n; i++) {
-    var color = map(i, 360, 0, 0, n * 2);
+  for (let i = 0; i < n; i++) {
+    const color = map(i, 360, 0, 0, n * 2);
     stroke(color, 80, 80, 0.9);
-    var inc = i * 0.5;
+    const inc = i * 0.5;
     line(x3(t + inc), y3(t + inc), x4(t + inc), y4(t + inc));
   }
 
-  t += map(mouseX, 0, width, -4, 4);
+  // t += map(mouseX, 0, width, -maxSpeed, maxSpeed);
+  t += 0.5;
 }
 
 function x1(t) {
-  return sin(t / 10) * 300 + sin(t / 10) * 20;
+  return sin(t / 10) * 400 + sin((t / 10) * b) * 20 * a;
 }
 
 function y1(t) {
-  return cos(t / 10) * 100;
+  return cos(t / 10 / a) * 100 * b;
 }
 
 function x2(t) {
-  return sin(t / 10) * 300 + sin(t) * 2;
+  return sin(t / 10) * 300 * a + (sin(t) * 2) / b;
 }
 
 function y2(t) {
-  return cos(t / 20) * 100 + cos(t / 12) * 200;
+  return cos(t / 20) * 100 + (cos(t / 12) * 200 * b) / a;
 }
 
 function x3(t) {
-  return sin(t / 5) * 200 + sin(t / 7) * 50;
+  return sin(t / 5) * 200 * a + sin(t / 7) * 50;
 }
 
 function y3(t) {
-  return cos(t / 10) * 300;
+  return cos(t / 10) * 300 * a;
 }
 
 function x4(t) {
-  return sin(t / 10) * 300 + sin(t / 45) * 2;
+  return sin(t / 10) * 300 * b + sin(t / 45) * 2;
 }
 
 function y4(t) {
-  return cos(t / 20) * 100 + cos(t / 19) * 200;
+  return cos(t / 20 / b) * 100 + cos(t / 19) * 200;
 }
