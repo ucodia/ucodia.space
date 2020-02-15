2d45ff59c3df07748fafca5ba930c5d85af8e127
Lionel Ringenbach <lionel@ucodia.space>
2020-02-15 13:22:33 -0800

    Prevent scrolling p5 canvas on mobile

--- src/components/P5Wrapper.js
+++ src/components/P5Wrapper.js
@@ -2,12 +2,17 @@ import React, { useRef, useEffect, useState } from "react";
 import p5 from "p5";
 
 const P5Wrapper = ({ sketch }) => {
-  // eslint-disable-next-line no-unused-vars
-  const [p5Instance, setP5Instance] = useState(null);
+  const [, setP5Instance] = useState(null);
   const p5Container = useRef(null);
   useEffect(() => {
     if (p5Container.current) {
       const newP5Instance = new p5(sketch, p5Container.current);
+
+      // prevent scrolling on mobile
+      p5Container.current.addEventListener("touchmove", event =>
+        event.preventDefault()
+      );
+
       setP5Instance(newP5Instance);
     }
   }, [sketch]);
