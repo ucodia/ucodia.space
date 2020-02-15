6cad0d38e7373b768533f6020a96ccfa8a6fa6c7
Lionel Ringenbach <lionel@ucodia.space>
2020-02-15 14:59:06 -0800

    Removed redux

--- src/components/RandomSketch.js
+++ src/components/RandomSketch.js
@@ -3,12 +3,13 @@ import FullScreen from "./FullScreen";
 import P5Wrapper from "./P5Wrapper";
 import sketches from "../sketches";
 
-const RandomSketch = () => {
-  const sketch = sketches[Math.floor(Math.random() * sketches.length)];
+const pickRandomSketch = () =>
+  sketches[Math.floor(Math.random() * sketches.length)];
 
+const RandomSketch = () => {
   return (
     <FullScreen>
-      <P5Wrapper sketch={sketch} />
+      <P5Wrapper sketch={pickRandomSketch()} />
     </FullScreen>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 import { debounce } from "lodash";
-import configureStore from "./store";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import setViewportVariables from "./utils/setViewportVariables";
@@ -20,17 +18,13 @@ const StyleReset = createGlobalStyle`
 setViewportVariables();
 window.addEventListener("resize", debounce(setViewportVariables, 400));
 
-const store = configureStore();
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <>
-        <StyleReset />
-        <App />
-      </>
-    </Router>
-  </Provider>,
+  <Router>
+    <>
+      <StyleReset />
+      <App />
+    </>
+  </Router>,
   document.getElementById("root")
 );
 
