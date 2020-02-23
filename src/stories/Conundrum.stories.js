f9ab7e1419f9ef33c3a0545eecd75cecd87fdf8d
Lionel Ringenbach <lionel@ucodia.space>
2020-02-22 23:01:05 -0800

    Added storybook

---
+++ src/stories/Conundrum.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Conundrum from "../apps/Conundrum";
+
+export default {
+  title: "Conundrum",
+  component: Conundrum
+};
+
+export const Showcase = () => <Conundrum />;
