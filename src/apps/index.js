c8254d01265c78147941f124f7658b8058c481da
Lionel Ringenbach <lionel@ucodia.space>
2020-03-08 00:42:01 -0800

    Fixing previous commit

--- src/apps/index.js
+++ src/apps/index.js
@@ -5,7 +5,6 @@ import Zukunft from "./Zukunft";
 import Flowtime from "./Flowtime";
 import Conundrum from "./Conundrum";
 import sketches from "./sketches";
-import Drawbot from "./Drawbot/Drawbot";
 import ThirtySixDaysOfType from "./36DaysOfType";
 
 export default {
@@ -14,7 +13,6 @@ export default {
   zukunft: <Zukunft />,
   flowtime: <Flowtime />,
   conundrum: <Conundrum />,
-  drawbot: <Drawbot />,
   "36daysoftype": <ThirtySixDaysOfType />,
   ...sketches
 };
