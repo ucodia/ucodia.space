2f1205cb68b34390b592893b37f021b024033942
Lionel Ringenbach <lionel@ucodia.space>
2020-03-09 23:41:32 -0700

    Added 36 days of type story

---
+++ src/stories/36DaysOfType.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ThirthySixDaysOfType from "../apps/36DaysOfType";
+import withFullScreen from "./decorators/withFullScreen";
+
+export default {
+  title: "36DaysOfType",
+  component: ThirthySixDaysOfType,
+  decorators: [withFullScreen]
+};
+
+export const Showcase = () => <ThirthySixDaysOfType />;
