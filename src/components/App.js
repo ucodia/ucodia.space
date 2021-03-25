23c3bc1c024cdf141e22fff23a4b6fad3b02066f
Lionel Ringenbach <lionel@ucodia.space>
2021-03-25 13:02:21 -0700

    Added EXP app listing

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import FullScreen from "./FullScreen";
-import RandomApp from "./RandomApp";
 import Home from "./Home";
+import Exp from "./Exp";
 
 const App = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route
-        path="/:appId?"
-        component={() => (
-          <FullScreen>
-            <RandomApp />
-          </FullScreen>
-        )}
-      />
+      <Route path="/exp" component={Exp} />
     </Switch>
   );
 };
---
+++ src/components/Exp.js
@@ -0,0 +1,13 @@
+const Exp = () => {
+  return (
+    <ul>
+      <li>
+        <a href="https://darel.d2qbrq58z9u0f1.amplifyapp.com/">
+          Dimensional Dunes (VR)
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default Exp;
