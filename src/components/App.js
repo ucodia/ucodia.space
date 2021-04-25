f08e84dcb842fe95c75433293388955b5ed8dd23
Lionel Ringenbach <lionel@ucodia.space>
2021-04-24 22:50:51 -0700

    Revert "Added EXP app listing"
    This reverts commit 23c3bc1c024cdf141e22fff23a4b6fad3b02066f.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import FullScreen from "./FullScreen";
+import RandomApp from "./RandomApp";
 import Home from "./Home";
-import Exp from "./Exp";
 
 const App = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/exp" component={Exp} />
+      <Route
+        path="/:appId?"
+        component={() => (
+          <FullScreen>
+            <RandomApp />
+          </FullScreen>
+        )}
+      />
     </Switch>
   );
 };
