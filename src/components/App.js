f06b854c2e8cbc53c9cdf75bd6ab9990b722412a
Lionel Ringenbach <lionel@ucodia.space>
2021-04-28 12:30:28 -0700

    Add redirection to Hic et Nunc

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import FullScreen from "./FullScreen";
 import RandomApp from "./RandomApp";
 import Home from "./Home";
 
+const GoToHEN = () => {
+  useEffect(() => {
+    window.location =
+      "https://www.hicetnunc.xyz/tz/tz1cMugfiPv184Dw6PUhai78dTnzvPx9jnDy";
+  }, []);
+  return <div>Redirecting to Hic et Nunc...</div>;
+};
+
 const App = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route
+        path="/hen"
+        exact
+        component={() => (
+          <FullScreen>
+            <GoToHEN />
+          </FullScreen>
+        )}
+      />
       <Route
         path="/:appId?"
         component={() => (
