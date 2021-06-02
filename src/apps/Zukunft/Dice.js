79552087d592767ff94384cc097ed159f53118e0
Lionel Ringenbach <ucodia@live.com>
2021-06-02 00:34:55 -0700

    Zukunft v2 with canvas editor and multi-function toolbar: - Brush selector - Clear canvas - Randomize canvas - Export to SVG

--- src/apps/Zukunft/Dice.js
+++ src/apps/Zukunft/Dice.js
@@ -19,7 +19,9 @@ const faceToDots = {
   t: [0, 1, 2, 4, 7],
   u: [0, 2, 3, 5, 6, 7, 8],
   z: [0, 1, 2, 4, 6, 7, 8],
+  "-": [3, 4, 5],
   "+": [1, 3, 4, 5, 7],
+  "carret-down": [3, 4, 5, 7],
 };
 
 const Surface = styled.g`
@@ -31,18 +33,30 @@ const Outline = styled.rect`
   stroke-width: 1px;
 `;
 const Dot = styled.ellipse`
-  fill: white;
+  fill: ${(props) => (props.primary ? "goldenrod" : "white")};
   stroke: none;
 `;
-const Dice = ({ face, size, transform, onClick = () => {} }) => {
+const Dice = ({
+  face,
+  size,
+  transform,
+  primary = false,
+  onClick = () => {},
+}) => {
   return (
-    <Surface transform={transform} onClick={onClick}>
-      <Outline width={size} height={size} rx={size / 5} ry={size / 5} />
+    <Surface transform={transform} onClick={onClick} className={`dice${face}`}>
+      <Outline
+        width={size}
+        height={size}
+        rx={size / 5}
+        ry={size / 5}
+        primary={primary}
+      />
       {faceToDots[face].map((dot, i) => {
         const cx = (size / 4) * ((dot % 3) + 1);
         const cy = (size / 4) * (Math.floor(dot / 3) + 1);
         const r = size / 10;
-        return <Dot key={i} cx={cx} cy={cy} rx={r} ry={r} />;
+        return <Dot key={i} cx={cx} cy={cy} rx={r} ry={r} primary={primary} />;
       })}
     </Surface>
   );
--- src/apps/Zukunft/Zukunft.js
+++ src/apps/Zukunft/Zukunft.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import styled from "styled-components";
 import { withSize } from "react-sizeme";
+import d3SaveSvg from "d3-save-svg";
 import getN from "../../utils/getN";
 import id from "../../utils/id";
 import randomInt from "../../utils/randomInt";
@@ -17,28 +18,39 @@ const Plot = styled.svg`
   height: 100%;
 `;
 
-const randomDice = () => ({
-  face: randomInt(1, 6),
-  orientation: randomInt(0, 3),
+const buttonFaces = [1, "-", "+", "carret-down"];
+const createDice = (face = 1, orientation = 0, primary = false) => ({
+  face,
+  orientation,
+  primary,
   id: id(),
 });
+const randomDice = () => createDice(randomInt(1, 6), randomInt(0, 1));
+const zeroDice = () => createDice(0, 0);
 
 const Zukunft = ({ size }) => {
+  const svgRef = useRef(null);
   const diceSize = useMemo(() => {
     if (size.width >= 1024) return 40;
     else if (size.width >= 480) return 50;
     else return 55;
   }, [size]);
   const [grid, setGrid] = useState({ dices: [], columns: 0, rows: 0 });
+
   useEffect(() => {
     setGrid((g) => {
-      const columns = Math.floor(size.width / diceSize);
-      const rows = Math.floor(size.height / diceSize);
+      const columns = Math.floor(size.width / diceSize) - 1;
+      const rows = Math.floor(size.height / diceSize) - 1;
       const diff = columns * rows - g.columns * g.rows;
       const dices =
         diff <= 0
           ? g.dices.slice(0, diff + g.dices.length)
           : [...g.dices, ...getN(diff, randomDice)];
+
+      // define buttons faces
+      buttonFaces.forEach((face, i) => {
+        dices[i] = createDice(face, 0, true);
+      });
 
       return { dices, columns, rows };
     });
@@ -47,16 +59,48 @@ const Zukunft = ({ size }) => {
   const handleDiceClick = (diceIndex) => {
     const dices = grid.dices.slice();
     const current = dices[diceIndex];
-    dices[diceIndex] = { ...randomDice(), face: (current.face + 1) % 7 };
+    dices[diceIndex] = {
+      ...current,
+      face: dices[0].face,
+      orientation: (current.orientation + 1) % 2,
+    };
     setGrid((g) => ({ ...g, dices }));
   };
-  const handleRandomize = () => {
-    setGrid((g) => ({ ...g, dices: getN(g.columns * g.rows, randomDice) }));
+  const handleCycleFace = (diceIndex) => {
+    const dices = grid.dices.slice();
+    const current = dices[diceIndex];
+    dices[diceIndex] = { ...current, face: (current.face + 1) % 7 };
+    setGrid((g) => ({ ...g, dices }));
+  };
+  const handleClearCanvas = () => {
+    setGrid((g) => {
+      const dices = [
+        ...g.dices.slice(0, buttonFaces.length),
+        ...getN(g.dices.length - buttonFaces.length, zeroDice),
+      ];
+      return { ...g, dices };
+    });
+  };
+  const handleRandomizeCanvas = () => {
+    setGrid((g) => {
+      const dices = [
+        ...g.dices.slice(0, buttonFaces.length),
+        ...getN(g.dices.length - buttonFaces.length, randomDice),
+      ];
+      return { ...g, dices };
+    });
+  };
+  const handleDownloadSvg = () => {
+    const suggestion = `zukunft-${new Date().getTime()}`;
+    const filename = prompt("Choose a filename:", suggestion);
+    if (filename) {
+      d3SaveSvg.save(svgRef.current, { filename });
+    }
   };
 
   return (
     <Container>
-      <Plot>
+      <Plot ref={svgRef}>
         {grid.dices.map((dice, i) => {
           const x = i % grid.columns;
           const xOff = (size.width - grid.columns * diceSize) / 2;
@@ -68,15 +112,25 @@ const Zukunft = ({ size }) => {
           const rotate = `rotate(${dice.orientation * 90}, ${diceSize / 2}, ${
             diceSize / 2
           })`;
-          const isFirstDice = i === 0;
 
           return (
             <Dice
               key={dice.id}
-              face={isFirstDice ? "o" : dice.face}
+              face={dice.face}
               size={diceSize}
               transform={`${translate} ${rotate}`}
-              onClick={isFirstDice ? handleRandomize : () => handleDiceClick(i)}
+              primary={dice.primary}
+              onClick={
+                i === 0
+                  ? () => handleCycleFace(0)
+                  : i === 1
+                  ? () => handleClearCanvas()
+                  : i === 2
+                  ? () => handleRandomizeCanvas()
+                  : i === 3
+                  ? () => handleDownloadSvg()
+                  : () => handleDiceClick(i)
+              }
             />
           );
         })}
@@ -85,4 +139,9 @@ const Zukunft = ({ size }) => {
   );
 };
 
-export default withSize({ monitorHeight: true })(Zukunft);
+export default withSize({
+  monitorHeight: true,
+  refreshRate: 100,
+  refreshMode: "debounce",
+  // noPlaceholder: true,
+})(Zukunft);
