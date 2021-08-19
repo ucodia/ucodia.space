6d7133a4ebbf5eb1a235eba5ee695128ef9a08c1
Lionel Ringenbach <ucodia@live.com>
2021-08-18 17:46:49 -0700

    Disable 36 days of type for now

--- src/apps/index.js
+++ src/apps/index.js
@@ -5,7 +5,7 @@ import Zukunft from "./Zukunft";
 import Flowtime from "./Flowtime";
 import Conundrum from "./Conundrum";
 import sketches from "./sketches";
-import ThirtySixDaysOfType from "./36DaysOfType";
+// import ThirtySixDaysOfType from "./36DaysOfType";
 import Lorenz from "./Lorenz";
 
 const apps = {
@@ -14,7 +14,11 @@ const apps = {
   zukunft: <Zukunft />,
   flowtime: <Flowtime />,
   conundrum: <Conundrum />,
-  "36daysoftype": <ThirtySixDaysOfType />,
+  // TODO: The UI of this one could use some love,
+  //       it's kind of a confusing experience a the moment
+  //       It would be nice to be able to type entire words,
+  //       pick a different style and some instructions
+  // "36daysoftype": <ThirtySixDaysOfType />,
   lorenz: <Lorenz />,
   ...sketches,
 };
