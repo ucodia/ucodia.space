05922cf415847e9ed4401a2b6e45b1d94e011048
Lionel Ringenbach <ucodia@live.com>
2021-10-23 00:36:34 -0700

    [refactor] Remove random app and HEN redirect

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,22 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import FullScreen from "./FullScreen";
-import RandomApp from "./RandomApp";
 import Home from "./Home";
-
-const GoToHEN = () => {
-  useEffect(() => {
-    window.location = "https://www.hicetnunc.xyz/ucodia";
-  }, []);
-  return <div>Redirecting to Hic et Nunc...</div>;
-};
+import apps from "../apps";
 
 const App = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route
-        path="/hen"
-        exact
-        component={() => (
-          <FullScreen>
-            <GoToHEN />
-          </FullScreen>
-        )}
-      />
-      <Route
-        path="/:appId?"
-        component={() => (
-          <FullScreen>
-            <RandomApp />
-          </FullScreen>
-        )}
-      />
+      {Object.keys(apps).map((appKey) => {
+        return (
+          <Route
+            key={appKey}
+            path={`/${appKey}`}
+            component={() => <FullScreen>{apps[appKey]}</FullScreen>}
+          />
+        );
+      })}
     </Switch>
   );
 };
