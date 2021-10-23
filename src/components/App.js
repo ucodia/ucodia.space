b84ac763ff76e777fa1a092768c842895d2fe42e
Lionel Ringenbach <ucodia@live.com>
2021-10-23 00:49:03 -0700

    Added 404 page

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import FullScreen from "./FullScreen";
+import NotFound from "./NotFound";
 import Home from "./Home";
 import apps from "../apps";
 
@@ -10,13 +11,16 @@ const App = () => {
       <Route path="/" exact component={Home} />
       {Object.keys(apps).map((appKey) => {
         return (
-          <Route
-            key={appKey}
-            path={`/${appKey}`}
-            component={() => <FullScreen>{apps[appKey]}</FullScreen>}
-          />
+          <Route key={appKey} path={`/${appKey}`}>
+            <FullScreen>{apps[appKey]}</FullScreen>
+          </Route>
         );
       })}
+      <Route path="*">
+        <FullScreen>
+          <NotFound />
+        </FullScreen>
+      </Route>
     </Switch>
   );
 };
---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Ceci n'est pas une page.</p>
+    </div>
+  );
+};
+
+export default NotFound;
