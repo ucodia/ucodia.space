642feb73cec3a95d6bd9c25651fadcfa7ea38c6c
Lionel Ringenbach <ucodia@live.com>
2021-10-23 00:11:52 -0700

    Remove Google analytics 👋

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme, darkTheme } from "../themes";
 import usePrefersDarkMode from "../hooks/usePrefersDarkMode";
-import RouterAnalytics from "./RouterAnalytics";
-import { IS_PROD } from "../utils/constants";
 
 const AppContext = ({ children }) => {
   const prefersDarkMode = usePrefersDarkMode();
@@ -12,13 +10,7 @@ const AppContext = ({ children }) => {
 
   return (
     <Router>
-      {IS_PROD ? (
-        <RouterAnalytics gaTrackingId="UA-159366089-1">
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
-        </RouterAnalytics>
-      ) : (
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
-      )}
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </Router>
   );
 };
