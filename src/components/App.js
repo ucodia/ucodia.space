4e58dac408b36aabc29b81d52812908b012125ad
Lionel Ringenbach <ucodia@live.com>
2021-10-25 23:56:33 -0700

    [feat] Add page titles

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,34 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import FullScreen from "./FullScreen";
-import NotFound from "./NotFound";
+import Page from "./Page";
 import Home from "./Home";
+import NotFound from "./NotFound";
 import apps from "../apps";
 
 const App = () => {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Route path="/" exact>
+        <Page title="You're Home">
+          <Home />
+        </Page>
+      </Route>
       {Object.keys(apps).map((appKey) => {
         return (
           <Route key={appKey} path={`/${appKey}`}>
-            <FullScreen>{apps[appKey]}</FullScreen>
+            <Page title={appKey}>
+              <FullScreen>{apps[appKey]}</FullScreen>
+            </Page>
           </Route>
         );
       })}
       <Route path="*">
-        <FullScreen>
-          <NotFound />
-        </FullScreen>
+        <Page title="404">
+          <FullScreen>
+            <NotFound />
+          </FullScreen>
+        </Page>
       </Route>
     </Switch>
   );
