85c383653f5c641b7bc1a15ce81696f5aaaa89f3
Lionel Ringenbach <ucodia@live.com>
2022-02-09 11:40:33 -0800

    Remove storybook, prettier and husky

--- src/stories/decorators/withFullScreen.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import FullScreen from "../../components/FullScreen";
-
-const withFullScreen = (story) => <FullScreen>{story()}</FullScreen>;
-
-export default withFullScreen;
