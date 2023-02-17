9d3b613bb6369f83d53de2a480efdef39047de42
Lionel Ringenbach <ucodia@live.com>
2023-02-16 19:20:46 -0800

    Create bundle chunk for P5

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  build: { sourcemap: true },
+  build: {
+    sourcemap: true,
+    rollupOptions: { output: { manualChunks: { p5: ["p5", "p5.js-svg"] } } },
+  },
 });
