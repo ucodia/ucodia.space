28ca4f93a60ebdc6372d4dec44841ba823f89b67
Lionel Ringenbach <ucodia@live.com>
2023-02-17 20:34:25 -0800

    Adding back to home button to 404

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+  color: #121212;
+  @media (prefers-color-scheme: dark) {
+    color: #ededed;
+  }
+`
+
 const NotFound = () => {
   return (
     <div>
       <h1>404</h1>
       <p>Ceci n'est pas une page.</p>
+      <StyledLink to="/">← Back to home</StyledLink>
     </div>
   );
 };
