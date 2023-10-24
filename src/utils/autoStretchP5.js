51e22325bd2305dcd0c2ea0699ea8cb3b9b9cf07
Lionel Ringenbach <ucodia@live.com>
2023-10-23 23:45:14 -0700

    Simplify auto strech function

--- src/utils/autoStretchP5.js
+++ src/utils/autoStretchP5.js
@@ -1,15 +1,13 @@
 import debounce from "./debounce";
 
-const stretchCanvas = (p5, onResized = () => {}) => {
-  p5.resizeCanvas(
-    p5.canvas.parentNode.clientWidth,
-    p5.canvas.parentNode.clientHeight
-  );
-  onResized();
-};
-
-const autoStretchP5 = (p5, onResized) => {
-  const stretch = () => stretchCanvas(p5, onResized);
+const autoStretchP5 = (p5, onResized = () => {}) => {
+  const stretch = () => {
+    p5.resizeCanvas(
+      p5.canvas.parentNode.clientWidth,
+      p5.canvas.parentNode.clientHeight
+    );
+    onResized();
+  };
   p5.windowResized = debounce(stretch, 400);
   stretch();
 };
