bd16bf20a5115605c7129673be72ae180c89421d
Lionel Ringenbach <ucodia@live.com>
2024-02-11 00:20:43 -0800

    Add infinite scrolling to look above

--- src/pages/sketches/lookAbove.js
+++ src/pages/sketches/lookAbove.js
@@ -4,104 +4,134 @@ export const meta = {
   slug: "look-above",
   name: "Look Above",
   created: "2015",
+  updated: "2024-02-09",
 };
 
 const lookAbove = (sketch) => {
-  var sky;
+  let stars = [];
+  let posX = 0;
+  let posY = 0;
+
+  const sx = {
+    universeSize: 1080 * 4,
+    starDensity: 0.001,
+    lightPolution: 0.1,
+  };
 
   sketch.setup = () => {
-    sketch.createCanvas(100, 100);
+    sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
+
+    const centerPos = Math.round(sx.universeSize / 2);
+    posX = centerPos;
+    posY = centerPos;
+
+    const starCount = Math.round(
+      sx.universeSize * sx.universeSize * sx.starDensity
+    );
+    generateStars(starCount);
+
+    autoStretchP5(sketch);
+  };
+
+  sketch.draw = () => {
+    sketch.background(0);
     sketch.noStroke();
-    sketch.noLoop();
+    sketch.fill(`hsl(0 ,0% ,${(1 - sx.lightPolution) * 100}%)`);
 
-    sky = new Sky();
-    autoStretchP5(sketch, () => sky.draw());
+    const deltaEdgeX = posX + sketch.width - sx.universeSize;
+    const deltaEdgeY = posY + sketch.height - sx.universeSize;
+
+    for (let i = 0; i < stars.length; i++) {
+      const star = stars[i];
+      const x = star.x - posX;
+      const y = star.y - posY;
+
+      // normal case
+      if (x > 0 && x < sketch.width && y > 0 && y < sketch.height) {
+        sketch.ellipse(x, y, star.starSize, star.starSize);
+      }
+      // beyond bottom right edge case
+      else if (
+        deltaEdgeX > 0 &&
+        star.x < deltaEdgeX &&
+        deltaEdgeY > 0 &&
+        star.y < deltaEdgeY
+      ) {
+        sketch.ellipse(
+          sketch.width - deltaEdgeX + star.x,
+          sketch.height - deltaEdgeY + star.y,
+          star.starSize,
+          star.starSize
+        );
+      }
+      // beyond right edge case
+      else if (deltaEdgeX > 0 && star.x < deltaEdgeX) {
+        sketch.ellipse(
+          sketch.width - deltaEdgeX + star.x,
+          y,
+          star.starSize,
+          star.starSize
+        );
+      }
+      // beyond bottom edge case
+      else if (deltaEdgeY > 0 && star.y < deltaEdgeY) {
+        sketch.ellipse(
+          x,
+          sketch.height - deltaEdgeY + star.y,
+          star.starSize,
+          star.starSize
+        );
+      }
+    }
+
+    // draw the edge of the universe
+    // sketch.stroke(255, 0, 0);
+    // sketch.line(
+    //   sketch.width - deltaEdgeX,
+    //   0,
+    //   sketch.width - deltaEdgeX,
+    //   sketch.height
+    // );
+    // sketch.line(
+    //   0,
+    //   sketch.height - deltaEdgeY,
+    //   sketch.width,
+    //   sketch.height - deltaEdgeY
+    // );
   };
 
-  sketch.mousePressed = () => {
-    sky.rebirth();
-    sky.draw();
-    return false;
+  sketch.mouseDragged = () => {
+    posX += sketch.pmouseX - sketch.mouseX;
+    posY += sketch.pmouseY - sketch.mouseY;
+    if (posX > sx.universeSize) posX -= sx.universeSize;
+    if (posX < 0) posX += sx.universeSize;
+    if (posY > sx.universeSize) posY -= sx.universeSize;
+    if (posY < 0) posY += sx.universeSize;
   };
 
-  // objects
-
-  function Sky() {
-    this.color = sketch.color(0, 12, 26);
-    this.density = 0;
-
-    // star size generator
-    this.rand = new RandomP();
-    this.rand.add(7, 9, 10000);
-    this.rand.add(5, 7, 1000);
-    this.rand.add(4, 5, 100);
-    this.rand.add(3, 4, 10);
-    this.rand.add(0, 3, 1);
-
-    this.rebirth = function () {
-      this.density = sketch.random(0.02, 0.1) / 1000;
-      var n = sketch.displayWidth * sketch.displayHeight * this.density;
-
-      this.stars = [];
-      for (var i = 0; i < n; i++) {
-        this.stars[i] = new Star(this.rand.next());
-      }
-    };
-
-    this.draw = function () {
-      sketch.background(this.color);
-
-      for (var i = 0; i < this.stars.length; i++) {
-        this.stars[i].draw();
-      }
-    };
-
-    this.rebirth();
+  function generateStars(n) {
+    stars = [];
+    for (let i = 0; i < n; i++) {
+      const x = randomInt(0, sx.universeSize);
+      const y = randomInt(0, sx.universeSize);
+      const starSize = getStarSize();
+      stars.push({ x: x, y: y, starSize });
+    }
   }
 
-  function Star(size) {
-    this.color = sketch.color(255);
-    this.x = 0;
-    this.y = 0;
-    this.size = size;
-
-    this.rebirth = function () {
-      this.x = sketch.random(sketch.displayWidth);
-      this.y = sketch.random(sketch.displayHeight);
-    };
-
-    this.draw = function () {
-      sketch.fill(this.color);
-      sketch.ellipse(this.x, this.y, this.size, this.size);
-    };
-
-    this.rebirth();
-  }
-
-  // tools
-
-  function RandomP() {
-    this.ranges = [];
-    this.n = 0;
-
-    this.add = function (min, max, p) {
-      this.ranges[this.n] = { min: min, max: max, p: p };
-      this.n++;
-    };
-
-    this.next = function () {
-      for (var i = 0; i < this.ranges.length; i++) {
-        var r = this.ranges[i];
-
-        if (randomInt(0, r.p - 1) === 0) {
-          return sketch.random(r.min, r.max);
-        }
-      }
-    };
+  function getStarSize() {
+    const n = Math.random();
+    if (n < 0.0001) return 7;
+    if (n < 0.001) return 6;
+    if (n < 0.01) return 5;
+    if (n < 0.1) return 4;
+    if (n < 0.25) return 3;
+    if (n < 0.5) return 2;
+    return 1;
   }
 
   function randomInt(min, max) {
-    return sketch.floor(sketch.random(min, max + 1));
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 };
 
