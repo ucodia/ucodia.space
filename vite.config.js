a94773a1c0ad4a824c1c8ab0d9e2e8b22187c3d7
Lionel Ringenbach <ucodia@live.com>
2024-02-11 22:47:15 -0800

    Add QR code for dev server hosting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import { qrcode } from "vite-plugin-qrcode";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), qrcode()],
   build: {
     sourcemap: true,
   },
