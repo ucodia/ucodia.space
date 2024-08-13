0062b6f32cb863474a280eb3830cd7dcd47ddfa4
Lionel Ringenbach <ucodia@live.com>
2024-08-13 17:57:40 -0400

    Disable image modal on small screens

--- src/components/CdnImageModal.jsx
+++ src/components/CdnImageModal.jsx
@@ -7,18 +7,28 @@ const CdnImageModal = ({ src, alt = "" }) => {
   const fullSrc = `${CDN_URL}/img${src}`;
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogTrigger asChild>
-        <img
-          src={fullSrc}
-          alt={alt}
-          className="cursor-pointer hover:opacity-90 transition-opacity"
-        />
-      </DialogTrigger>
-      <DialogContent className="max-w-[95vw] max-h-[95vh] w-auto h-auto p-0 border-none bg-transparent">
-        <img src={fullSrc} alt={alt} className="w-full h-full object-contain" />
-      </DialogContent>
-    </Dialog>
+    <>
+      {/* Mobile: Regular image */}
+      <img src={fullSrc} alt={alt} className="w-full h-auto md:hidden" />
+
+      {/* Desktop: Modal functionality */}
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger asChild>
+          <img
+            src={fullSrc}
+            alt={alt}
+            className="hidden md:block cursor-pointer hover:opacity-90 transition-opacity"
+          />
+        </DialogTrigger>
+        <DialogContent className="max-w-[95vw] max-h-[95vh] w-auto h-auto p-0 border-none bg-transparent">
+          <img
+            src={fullSrc}
+            alt={alt}
+            className="w-full h-full object-contain"
+          />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
