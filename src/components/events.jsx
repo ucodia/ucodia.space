764fb9acddf810302ffc8f3b02f6b7906aa5ad62
Lionel Ringenbach <ucodia@live.com>
2024-11-06 14:16:17 -0800

    Update events and display

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -5,12 +5,16 @@ const events = [
     start: new Date("2024-01-06"),
     name: "New Year, New Art - Group Exhbit",
     location: "Slice of Life, 1636 Venables St, Vancouver, BC V5L 2H2",
+    description:
+      "Join me for my first exibit and artistic debut on this new year!",
   },
   {
     start: new Date("2024-11-14"),
     end: new Date("2024-11-17"),
-    name: "Culture Crawl",
+    name: "Human + Machine live installation - Culture Crawl Exhibit",
     location: "Slice of Life, 1636 Venables St, Vancouver, BC V5L 2H2",
+    description:
+      "Come check out unique algorithmic art coming to life as I collaborate with my trusty drawing machine. You may even be able to create your own and bring it home!.",
   },
   {
     start: new Date("2025-04-12"),
@@ -20,18 +24,21 @@ const events = [
   {
     start: new Date("2025-06-12"),
     end: new Date("2025-06-15"),
-    name: "Man+Machine - Solo Exhibit",
+    name: "Code is a paintbrush 🎨 - Solo Exhibit",
     location: "Slice of Life, 1636 Venables St, Vancouver, BC V5L 2H2",
+    description:
+      "Discover a collection of works that started from code and travelled to our world through various means of transformation, it is a tale of what occurs when navigating between realms and the unique manifestations of machine co-creation.",
   },
 ];
 
 const now = new Date();
 
 const formatDate = (date) => {
-  return date.toLocaleDateString("en-US", {
+  return date.toLocaleString(navigator.language, {
     year: "numeric",
     month: "long",
     day: "numeric",
+    timeZone: "UTC",
   });
 };
 
@@ -48,6 +55,7 @@ const EventItem = ({ event }) => (
       {formatDate(event.start)}
       {event.end && ` - ${formatDate(event.end)}`}
     </p>
+    {event.description && <p className="italic">{event.description}</p>}
     <a
       href={getGoogleMapsUrl(event.location)}
       target="_blank"
