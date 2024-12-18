991262513f6273ad0d57686d9d4f2367406a90bf
Lionel Ringenbach <ucodia@live.com>
2024-12-17 18:09:07 -0800

    Scroll to top on page title change

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 const Page = (props) => {
   useEffect(() => {
     document.title = `${props.title} - Ucodia's space 🛸` || "";
+    window.scrollTo(0, 0);
   }, [props.title]);
   return props.children;
 };
