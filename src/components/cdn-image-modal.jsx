7e06f61d4052b1159b8daee54dad01aed200d3eb
Lionel Ringenbach <ucodia@live.com>
2024-12-17 18:20:31 -0800

    Make image modal wider on desktop

--- src/components/cdn-image-modal.jsx
+++ src/components/cdn-image-modal.jsx
@@ -20,11 +20,11 @@ const CdnImageModal = ({ src, alt = "" }) => {
             className="hidden md:block cursor-pointer hover:opacity-90 transition-opacity"
           />
         </DialogTrigger>
-        <DialogContent className="max-w-[95vw] max-h-[95vh] w-auto h-auto p-0 border-none bg-transparent">
+        <DialogContent className="max-w-[95vw] max-h-[95vh] p-0 border-none bg-transparent flex items-center justify-center">
           <img
             src={fullSrc}
             alt={alt}
-            className="w-full h-full object-contain"
+            className="w-auto h-auto max-w-[95vw] max-h-[95vh] object-contain"
           />
         </DialogContent>
       </Dialog>
