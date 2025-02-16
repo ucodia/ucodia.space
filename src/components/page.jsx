29f9193a586be3607c3ceabcab50ab43359ffabb
Lionel Ringenbach <ucodia@live.com>
2025-02-16 17:51:22 +0100

    refactor: rename files to lowercase

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 const Page = (props) => {
   useEffect(() => {
     document.title = `${props.title} - Ucodia's space 🛸` || "";
-    window.scrollTo(0, 0);
   }, [props.title]);
   return props.children;
 };
