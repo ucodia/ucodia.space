3406d12c864d9bf3978b55120e0bcefc2952c568
Lionel Ringenbach <ucodia@live.com>
2025-02-26 00:45:35 +0100

    Update SVG logo styling and sizing

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,17 +18,11 @@ const Home = () => {
         <div className="mr-6 h-[100px] w-[100px] sm:mr-[50px] sm:h-[200px] sm:w-[200px]">
           <U5Wrapper sketch={singleDiamond} />
         </div>
-        <picture>
-          <source
-            srcSet="/dark-ucodia-logo.svg"
-            media="(prefers-color-scheme: dark)"
-          />
-          <img
-            src="/light-ucodia-logo.svg"
-            alt="website logo"
-            className="h-[50px] sm:h-[100px]"
-          />
-        </picture>
+        <img
+          src="/ucodia-logo.svg"
+          alt="website logo"
+          className="h-[50px] w-[155px] sm:h-[100px] sm:w-[310px] dark:invert"
+        />
       </div>
       <div className="my-12 sm:my-[50px] flex flex-col items-center justify-center">
         {links.map(({ name, to }, index, items) => {
