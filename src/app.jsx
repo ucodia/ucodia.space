8ccb8ed3a065a1ec5c2ef31398ebfe22c5f6008d
Lionel Ringenbach <ucodia@live.com>
2025-02-26 02:01:45 +0100

    Revert to previous router style to reduce bundle size

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/theme";
 import Page from "@/components/page";
 import Alert from "@/components/alert";
@@ -12,35 +12,44 @@ const components = {
   a: ExternalLink,
 };
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <Page title="You're Home">
-        <Home />
-      </Page>
-    ),
-    errorElement: (
-      <Page title="404">
-        <div className="w-screen h-screen flex items-center justify-center">
-          <Alert title="404">
-            <p>Ceci n'est pas une page.</p>
-          </Alert>
-        </div>
-      </Page>
-    ),
-  },
-  ...routes.map(({ name, path, element }) => ({
-    path,
-    element: <Page title={name}>{element}</Page>,
-  })),
-]);
-
 const App = () => {
   return (
     <ThemeProvider>
       <MDXProvider components={components}>
-        <RouterProvider router={router} />
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              exact
+              element={
+                <Page title="You're Home">
+                  <Home />
+                </Page>
+              }
+            />
+            {routes.map(({ path, name, element }, i) => {
+              return (
+                <Route
+                  key={path}
+                  path={path}
+                  element={<Page title={name}>{element}</Page>}
+                />
+              );
+            })}
+            <Route
+              path="*"
+              element={
+                <Page title="404">
+                  <div className="w-screen h-screen flex items-center justify-center">
+                    <Alert title="404">
+                      <p>Ceci n'est pas une page.</p>
+                    </Alert>
+                  </div>
+                </Page>
+              }
+            />
+          </Routes>
+        </Router>
       </MDXProvider>
     </ThemeProvider>
   );
